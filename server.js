eebf7ea4c1518aa8daaa362c3e998ad395055c0f
Tulio Di Sabatino <tuliosabatino@gmail.com>
2017-05-31 21:48:34 +0000

    TimeStamp working for Unix input

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+function parsetime (time) {
+  var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  
+  var naturalDate = JSON.stringify(months[time.getMonth()] + ' ' + time.getDate() + ', ' + time.getFullYear());
+  return naturalDate;
+}
+
+function unixtime (time) {
+  return time.getTime();
+}
+
+function bothTimes (unixTime, naturalTime) {
+    return { unix: JSON.parse(unixTime), natural: JSON.parse(naturalTime) };
+}
+
+app.get('/:time', function (req, res) {
+  var unixDate = new Date(parseInt(req.params.time,0));
+  var normalDate = new Date(parseInt(req.params.time*1000,0));
+  
+  var unix_time = unixtime(unixDate);
+  var normal_time = parsetime(normalDate);
+  
+  var finalTime = bothTimes(unix_time, normal_time);
+  
+  res.send( finalTime );
+});
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Test app on https://api-projects-sabathh.c9users.io/1515455516');
 })
